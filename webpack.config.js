e21ac38d8332e18fc02427c6121a15176d633e1d
Matt Hinchliffe <matt@maketea.co.uk>
2016-03-27 18:57:52 +0100

    Compile demo bundle without React dev cruft.

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,14 @@ module.exports = {
     devtool: 'source-map',
     module: {
         loaders: [
-            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ }
         ]
     },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': { 'NODE_ENV': '"production"' }
+        })
+    ],
     output: {
         filename: "example/bundle.js"
     }
