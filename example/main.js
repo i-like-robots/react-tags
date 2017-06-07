d25a6998c369bb8842bfb7123ed66b02daa53ed4
Matt Hinchliffe <matt.hinchliffe@ft.com>
2017-06-07 11:49:45 +0100

    Remove broken example code

--- example/main.js
+++ example/main.js
@@ -33,7 +33,6 @@ class App extends React.Component {
     return (
       <div>
         <Tags
-          delimiters={'asdasd'}
           tags={this.state.tags}
           suggestions={this.state.suggestions}
           handleDelete={this.handleDelete.bind(this)}
