fc17cd0a26fe6b8a24f81d995f5ce34278e0625b
Matt Hinchliffe <matt@maketea.co.uk>
2020-02-09 11:45:21 +0000

    Update all Rollup dependencies to latest versions

--- example/rollup.config.js
+++ example/rollup.config.js
@@ -1,9 +1,9 @@
-import buble from 'rollup-plugin-buble'
+import buble from '@rollup/plugin-buble'
 import serve from 'rollup-plugin-serve'
-import replace from 'rollup-plugin-replace'
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
-import { uglify } from 'rollup-plugin-uglify'
+import replace from '@rollup/plugin-replace'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
 
 const plugins = [
   // Use Buble plugin to transpile JSX
@@ -22,8 +22,8 @@ const plugins = [
 ]
 
 if (process.env.NODE_ENV === 'production') {
-  // Use Uglify plugin to minify output
-  plugins.push(uglify())
+  // Use Terser plugin to minify output
+  plugins.push(terser())
 } else {
   plugins.push(serve({ open: true, contentBase: 'example', port: 8080 }))
 }
