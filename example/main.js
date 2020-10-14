d76b070444f0dc624afb5d4473f13a8e1c9573f3
Matt Hinchliffe <matt.hinchliffe@ft.com>
2020-10-14 18:24:39 +0100

    Add second example demonstrating new tags behaviour

--- example/main.js
+++ example/main.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import ReactTags from '../lib/ReactTags'
 import suggestions from './countries'
 
+/**
+ * Demo 1 - Country selector
+ */
 class Demo1 extends React.Component {
   constructor (props) {
     super(props)
@@ -48,3 +51,51 @@ class Demo1 extends React.Component {
 }
 
 ReactDOM.render(<Demo1 />, document.getElementById('demo-1'))
+
+/**
+ * Demo 2 - Custom tags
+ */
+class Demo2 extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      tags: [],
+      suggestions: []
+    }
+
+    this.reactTags = React.createRef()
+  }
+
+  onDelete (i) {
+    const tags = this.state.tags.slice(0)
+    tags.splice(i, 1)
+    this.setState({ tags })
+  }
+
+  onAddition (tag) {
+    const tags = [].concat(this.state.tags, tag)
+    this.setState({ tags })
+  }
+
+  render () {
+    return (
+      <>
+        <p>Enter new tags using React Tags below:</p>
+        <ReactTags
+          allowNew={true}
+          newTagPrefix="Create new tag: "
+          ref={this.reactTags}
+          tags={this.state.tags}
+          suggestions={this.state.suggestions}
+          onDelete={this.onDelete.bind(this)}
+          onAddition={this.onAddition.bind(this)}
+        />
+        <p>Output:</p>
+        <pre><code>{JSON.stringify(this.state.tags, null, 2)}</code></pre>
+      </>
+    )
+  }
+}
+
+ReactDOM.render(<Demo2 />, document.getElementById('demo-2'))
